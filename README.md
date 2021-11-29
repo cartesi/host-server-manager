476b23d2d167687c1083e9a0ddfd5e46459eb9b9
Gabriel Q Ligneul <gabriel.ligneul@cartesi.io>
2021-11-29 17:04:59 -0300

    Add configuration from env-vars and READ-ME

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# mock-rollups-machine-manager
+# Mock Rollup Machine Manager
+
+This project simulates the gRPC API provided by the Rollups Machine Manager Container.
+Instead of instantiating a Cartesi Machine for the DApp, the Mock makes HTTP requests to a DApp that should be running in the host machine.
+This project also simulates the Inspect API provided by the Inspect Container.
+
+## Getting Started
+
+This project requires Rust.
+To install Rust follow the instructions [here](https://www.rust-lang.org/tools/install).
+
+## Running
+
+To run the mock, execute the command:
+
+```
+cargo run
+```
+
+## Configuration
+
+It is possible to configure the server behaviour with environment variables.
+
+- `CARTESI_GRPC_MACHINE_MANAGER_ADDRESS`
+
+gRPC address of the Mock Rollups Machine Manager endpoint (default = `127.0.0.1`).
+
+- `CARTESI_GRPC_MACHINE_MANAGER_PORT`
+
+gRPC port of the Mock Rollups Machine Manager endpoint (default = `5000`).
+
+- `CARTESI_HTTP_TARGET_PROXY_ADDRESS`
+
+HTTP address of the Target Proxy endpoint (default = `127.0.0.1`).
+This is the endpoint that receives HTTP requests from the DApp backend.
+
+- `CARTESI_HTTP_TARGET_PROXY_PORT`
+
+HTTP port of the Target Proxy endpoint (default = `5001`).
+
+- `CARTESI_HTTP_INSPECT_ADDRESS`
+
+HTTP address of the Inspect endpoint (default = `127.0.0.1`).
+This is the endpoint that receives the HTTP `/inspect` request from the DApp frontend.
+
+- `CARTESI_HTTP_INSPECT_PORT`
+
+HTTP port of the Inspect endpoint (default = `5002`).
+
+- `CARTESI_DAPP_HTTP_ADDRESS`
+
+HTTP address of the DApp backend (default = `127.0.0.1`).
+
+- `CARTESI_DAPP_HTTP_PORT`
+
+HTTP port of the DApp backend (default = `5003`).
+
+## Tests
+
+To run the tests, execute the command:
+
+```
+cargo tests
+```
--- src/config.rs
+++ src/config.rs
@@ -10,6 +10,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+use std::env;
+
 #[derive(Clone)]
 pub struct Config {
     pub grpc_machine_manager_address: String,
@@ -25,14 +27,27 @@ pub struct Config {
 impl Config {
     pub fn new() -> Self {
         Self {
-            grpc_machine_manager_address: String::from("127.0.0.1"),
-            grpc_machine_manager_port: 5000,
-            http_target_proxy_address: String::from("127.0.0.1"),
-            http_target_proxy_port: 5001,
-            http_inspect_address: String::from("127.0.0.1"),
-            http_inspect_port: 5002,
-            dapp_http_address: String::from("127.0.0.1"),
-            dapp_http_port: 5003,
+            grpc_machine_manager_address: get_address("CARTESI_GRPC_MACHINE_MANAGER_ADDRESS"),
+            grpc_machine_manager_port: get_port("CARTESI_GRPC_MACHINE_MANAGER_PORT", 5000),
+            http_target_proxy_address: get_address("CARTESI_HTTP_TARGET_PROXY_ADDRESS"),
+            http_target_proxy_port: get_port("CARTESI_HTTP_TARGET_PROXY_PORT", 5001),
+            http_inspect_address: get_address("CARTESI_HTTP_INSPECT_ADDRESS"),
+            http_inspect_port: get_port("CARTESI_HTTP_INSPECT_PORT", 5002),
+            dapp_http_address: get_address("CARTESI_DAPP_HTTP_ADDRESS"),
+            dapp_http_port: get_port("CARTESI_DAPP_HTTP_PORT", 5003),
         }
     }
+}
+
+fn get_address(var: &str) -> String {
+    env::var(var).unwrap_or(String::from("127.0.0.1"))
+}
+
+fn get_port(var: &str, default: u16) -> u16 {
+    env::var(var)
+        .map(|s| {
+            s.parse::<u16>()
+                .expect(format!("failed to parse variable {}", var).as_str())
+        })
+        .unwrap_or(default)
 }
